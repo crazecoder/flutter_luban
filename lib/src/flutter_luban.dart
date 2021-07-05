9756bbddb075c2878ee8d19dd4e6856a1486bced
Ahmed Nabil <ahmed.n.yagoub@gmail.com>
2021-07-05 20:19:44 +0300

    Added byte data compression

--- lib/src/flutter_luban.dart
+++ lib/src/flutter_luban.dart
@@ -14,6 +14,10 @@ class Luban {
     return compute(_lubanCompress, object);
   }
 
+  static Future<List<int>> compressRowImage(CompressObject object) async {
+    return compute(_lubanCompressRaw, object);
+  }
+
   static Future<dynamic> compressImageQueue(CompressObject object) async {
     final response = ReceivePort();
     await Isolate.spawn(_lubanCompressQueue, response.sendPort);
@@ -204,6 +208,138 @@ class Luban {
     return decodedImageFile.path;
   }
 
+  static List<int> _lubanCompressRaw(CompressObject object) {
+    Image image = decodeImage(object.imageBytes!)!;
+    final length = object.imageBytes!.length;
+    print(object.imageExt!);
+    bool isLandscape = false;
+    const List<String> jpgSuffix = ["jpg", "jpeg", "JPG", "JPEG"];
+    const List<String> pngSuffix = ["png", "PNG"];
+    bool isJpg = _parseType(object.imageExt!, jpgSuffix);
+    bool isPng = false;
+
+    if (!isJpg) isPng = _parseType(object.imageExt!, pngSuffix);
+
+    double size;
+    int fixelW = image.width;
+    int fixelH = image.height;
+    double thumbW = (fixelW % 2 == 1 ? fixelW + 1 : fixelW).toDouble();
+    double thumbH = (fixelH % 2 == 1 ? fixelH + 1 : fixelH).toDouble();
+    double scale = 0;
+    if (fixelW > fixelH) {
+      scale = fixelH / fixelW;
+      var tempFixelH = fixelW;
+      var tempFixelW = fixelH;
+      fixelH = tempFixelH;
+      fixelW = tempFixelW;
+      isLandscape = true;
+    } else {
+      scale = fixelW / fixelH;
+    }
+    List<int>? decodedImageFile;
+    if (isJpg)
+      print('Decoding JPG image');
+    else if (isPng)
+      print('Decoding PNG image');
+    else
+      throw Exception("flutter_luban don't support this image type");
+
+    var imageSize = length / 1024;
+    if (scale <= 1 && scale > 0.5625) {
+      if (fixelH < 1664) {
+        if (imageSize < 150) {
+          return encodeJpg(image, quality: object.quality);
+        }
+        size = (fixelW * fixelH) / pow(1664, 2) * 150;
+        size = size < 60 ? 60 : size;
+      } else if (fixelH >= 1664 && fixelH < 4990) {
+        thumbW = fixelW / 2;
+        thumbH = fixelH / 2;
+        size = (thumbH * thumbW) / pow(2495, 2) * 300;
+        size = size < 60 ? 60 : size;
+      } else if (fixelH >= 4990 && fixelH < 10240) {
+        thumbW = fixelW / 4;
+        thumbH = fixelH / 4;
+        size = (thumbW * thumbH) / pow(2560, 2) * 300;
+        size = size < 100 ? 100 : size;
+      } else {
+        int multiple = fixelH / 1280 == 0 ? 1 : fixelH ~/ 1280;
+        thumbW = fixelW / multiple;
+        thumbH = fixelH / multiple;
+        size = (thumbW * thumbH) / pow(2560, 2) * 300;
+        size = size < 100 ? 100 : size;
+      }
+    } else if (scale <= 0.5625 && scale >= 0.5) {
+      if (fixelH < 1280 && imageSize < 200) {
+        return encodeJpg(image, quality: object.quality);
+      }
+      int multiple = fixelH / 1280 == 0 ? 1 : fixelH ~/ 1280;
+      thumbW = fixelW / multiple;
+      thumbH = fixelH / multiple;
+      size = (thumbW * thumbH) / (1440.0 * 2560.0) * 200;
+      size = size < 100 ? 100 : size;
+    } else {
+      int multiple = (fixelH / (1280.0 / scale)).ceil();
+      thumbW = fixelW / multiple;
+      thumbH = fixelH / multiple;
+      size = ((thumbW * thumbH) / (1280.0 * (1280 / scale))) * 500;
+      size = size < 100 ? 100 : size;
+    }
+    if (imageSize < size) {
+      return encodeJpg(image, quality: object.quality);
+    }
+    Image smallerImage;
+    if (isLandscape) {
+      smallerImage = copyResize(image,
+          width: thumbH.toInt(),
+          height: object.autoRatio ? null : thumbW.toInt());
+    } else {
+      smallerImage = copyResize(image,
+          width: thumbW.toInt(),
+          height: object.autoRatio ? null : thumbH.toInt());
+    }
+
+    if (object.mode == CompressMode.LARGE2SMALL) {
+      return _large2SmallCompressRawImage(
+        image: smallerImage,
+        imageBytes: decodedImageFile,
+        quality: object.quality,
+        targetSize: size,
+        step: object.step,
+        isJpg: isJpg,
+      );
+    } else if (object.mode == CompressMode.SMALL2LARGE) {
+      return _small2LargeCompressRawImage(
+        image: smallerImage,
+        imageBytes: decodedImageFile,
+        quality: object.step,
+        targetSize: size,
+        step: object.step,
+        isJpg: isJpg,
+      );
+    } else {
+      if (imageSize < 500) {
+        return _large2SmallCompressRawImage(
+          image: smallerImage,
+          imageBytes: decodedImageFile,
+          quality: object.quality,
+          targetSize: size,
+          step: object.step,
+          isJpg: isJpg,
+        );
+      } else {
+        return _small2LargeCompressRawImage(
+          image: smallerImage,
+          imageBytes: decodedImageFile,
+          quality: object.step,
+          targetSize: size,
+          step: object.step,
+          isJpg: isJpg,
+        );
+      }
+    }
+  }
+
   static _large2SmallCompressImage({
     Image? image,
     File? file,
@@ -231,6 +367,38 @@ class Luban {
       _compressPng(
         image: image!,
         file: file,
+        targetSize: targetSize,
+        large2Small: true,
+      );
+    }
+  }
+
+  static List<int> _large2SmallCompressRawImage({
+    Image? image,
+    List<int>? imageBytes,
+    quality,
+    targetSize,
+    step,
+    bool isJpg: true,
+  }) {
+    if (isJpg) {
+      var im = encodeJpg(image!, quality: quality);
+      var tempImageSize = Uint8List.fromList(im).lengthInBytes;
+      if (tempImageSize / 1024 > targetSize && quality > step) {
+        quality -= step;
+        return _large2SmallCompressRawImage(
+          image: image,
+          imageBytes: imageBytes,
+          quality: quality,
+          targetSize: targetSize,
+          step: step,
+        );
+      }
+      return im;
+    } else {
+      return _compressRawPng(
+        image: image!,
+        imageBytes: imageBytes,
         targetSize: targetSize,
         large2Small: true,
       );
@@ -271,6 +439,39 @@ class Luban {
     }
   }
 
+  static List<int> _small2LargeCompressRawImage({
+    Image? image,
+    List<int>? imageBytes,
+    quality,
+    targetSize,
+    step,
+    bool isJpg: true,
+  }) {
+    if (isJpg) {
+      var im = encodeJpg(image!, quality: quality);
+      var tempImageSize = Uint8List.fromList(im).lengthInBytes;
+      if (tempImageSize / 1024 < targetSize && quality <= 100) {
+        quality += step;
+        return _small2LargeCompressRawImage(
+          image: image,
+          imageBytes: imageBytes,
+          quality: quality,
+          targetSize: targetSize,
+          step: step,
+          isJpg: isJpg,
+        );
+      }
+      return im;
+    } else {
+      return _compressRawPng(
+        image: image!,
+        imageBytes: imageBytes,
+        targetSize: targetSize,
+        large2Small: false,
+      );
+    }
+  }
+
   ///level 1~9  level++ -> image--
   static void _compressPng({
     required Image image,
@@ -303,6 +504,36 @@ class Luban {
 
     file!.writeAsBytesSync(im);
   }
+
+  static List<int> _compressRawPng({
+    required Image image,
+    List<int>? imageBytes,
+    level,
+    targetSize,
+    required bool large2Small,
+  }) {
+    var _level;
+    if (large2Small) {
+      _level = level ?? 1;
+    } else {
+      _level = level ?? 9;
+    }
+    List<int> im = encodePng(image, level: _level);
+    if (_level > 9 || _level < 1) {
+    } else {
+      var tempImageSize = Uint8List.fromList(im).lengthInBytes;
+      if (tempImageSize / 1024 > targetSize) {
+        return _compressRawPng(
+          image: image,
+          imageBytes: imageBytes,
+          targetSize: targetSize,
+          level: large2Small ? _level + 1 : _level - 1,
+          large2Small: large2Small,
+        );
+      }
+    }
+    return im;
+  }
 }
 
 enum CompressMode {
@@ -317,6 +548,8 @@ class CompressObject {
   final CompressMode mode;
   final int quality;
   final int step;
+  final Uint8List? imageBytes;
+  final String? imageExt;
 
   ///If you are not sure whether the image detail property is correct, set true, otherwise the compressed ratio may be incorrect
   final bool autoRatio;
@@ -328,5 +561,7 @@ class CompressObject {
     this.quality: 80,
     this.step: 6,
     this.autoRatio = true,
+    this.imageBytes,
+    this.imageExt,
   });
 }
